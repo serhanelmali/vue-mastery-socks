76fddc2cedb92d8ad83a5dcbc84caa6f5496bb6d
Serhan Elmalı <serhanelmali@gmail.com>
2021-10-25 19:15:15 +0300

    adds variants

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -3,17 +3,34 @@
     <div class="product-image">
       <img v-bind:src="image" />
     </div>
+
     <div class="product-info">
       <h1>{{ product }}</h1>
-      <p v-if="inventory > 10">In Stock</p>
-      <p v-else-if="inventory <= 10 && inventory > 0">Almost Sold Out!</p>
+      <p v-if="inStock">In Stock</p>
       <p v-else>Out of Stock</p>
+
+      <ul>
+        <li v-for="detail in details" :key="detail">{{ detail }}</li>
+      </ul>
+
+      <div v-for="variant in variants" :key="variant.variantId">
+        <p @mouseover="updateProduct(variant.variantImage)">
+          {{ variant.variantColor }}
+        </p>
+      </div>
+
+      <button v-on:click="addToCart">Add to Cart</button>
+
+      <div class="cart">
+        <p>Cart({{ cart }})</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import greenSock from "../assets/vmSocks-green.jpg";
+import blueSock from "../assets/vmSocks-blue.jpg";
 
 export default {
   name: "Products",
@@ -23,8 +40,23 @@ export default {
     return {
       product: "Socks",
       image: greenSock,
-      inventory: 11,
+      inStock: true,
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [
+        { variantId: 2234, variantColor: "green", variantImage: greenSock },
+        { variantId: 2235, variantColor: "blue", variantImage: blueSock },
+      ],
+      cart: 0,
     };
+  },
+
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    },
   },
 };
 </script>
@@ -32,8 +64,6 @@ export default {
 <style scoped>
 .product {
   display: flex;
-  flex-flow: wrap;
-  padding: 1rem;
 }
 
 img {
@@ -44,12 +74,56 @@ img {
 }
 
 .product-image {
-  width: 80%;
+  flex-basis: 700px;
 }
 
-.product-image,
 .product-info {
   margin-top: 10px;
-  width: 50%;
+  flex-basis: 500px;
+}
+
+.color-box {
+  width: 40px;
+  height: 40px;
+  margin-top: 5px;
+}
+
+.cart {
+  margin-right: 25px;
+  float: right;
+  border: 1px solid #d8d8d8;
+  padding: 5px 20px;
+}
+
+button {
+  margin-top: 30px;
+  border: none;
+  background-color: #1e95ea;
+  color: white;
+  height: 40px;
+  width: 100px;
+  font-size: 14px;
+  cursor: pointer;
+}
+
+.disabledButton {
+  background-color: #d8d8d8;
+}
+
+.review-form {
+  width: 30%;
+  padding: 20px;
+  border: 1px solid #d8d8d8;
+}
+
+input {
+  width: 100%;
+  height: 25px;
+  margin-bottom: 20px;
+}
+
+textarea {
+  width: 100%;
+  height: 60px;
 }
 </style>
